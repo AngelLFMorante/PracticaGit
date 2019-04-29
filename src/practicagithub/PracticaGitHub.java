86ad50ab7d2b5d230bb0c4ca2c4c3f0163959cf3
1_web9_13 <1_web9_13@A08_PC14.JOVELLANOS.EDU>
2019-04-29 16:09:38 +0200

    inicio Angel

--- src/practicagithub/PracticaGitHub.java
+++ src/practicagithub/PracticaGitHub.java
@@ -16,6 +16,7 @@ public class PracticaGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola estamos en el inicio");
     }
     
 }
